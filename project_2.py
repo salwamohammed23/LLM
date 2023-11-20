b0d2b3750542caeedafe54a0d57e0637a85d85ba
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2023-11-20 20:07:24 +0200

    Update project_2.py

--- project_2.py
+++ project_2.py
@@ -5,17 +5,7 @@ import io
 from transformers import pipeline
 import matplotlib.pyplot as plt
 
-hide_github_icon = “”"
 
-.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
-“”"
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
-API_URL = "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4"
-HEADERS = {"Authorization": f"Bearer {'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'}"}
-
-# Streamlit UI
-st.title("Image and Text Generation App")
 
 
 
